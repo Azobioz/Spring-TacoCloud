d1eb57023080ef14f2a73fb5123119f8a22b7206
Azobioz <145475512+Azobioz@users.noreply.github.com>
2024-09-04 15:14:31 +0300

    In memory users
    2 users: with admin role and user role

--- taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -1,11 +1,7 @@
 package tacos.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
@@ -17,15 +13,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import tacos.web.MyUserDetailsService;
 
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig implements WebMvcConfigurer {
 
-    @Autowired
-    private MyUserDetailsService userDetailsService;
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -50,34 +44,24 @@ public class SecurityConfig implements WebMvcConfigurer {
 
 
 
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        UserDetails normalUser = User.builder()
-//                        .username("NormalUser")
-//                        .password("$2a$12$92oMmW2pZUD9WGSk9EpBp.Z.9cD5dXK98j0UMJIM4XgcgQvsiWfSy") //12345
-//                        .roles("USER")
-//                        .build();
-//
-//        UserDetails adminUser = User.builder()
-//                        .username("AdminUser")
-//                        .password("$2a$12$b7zE4gzv0ugqjvMCquxuQeVGwJeZAGAWqvi8/Gpm.QWOlaD9wcsRO") // 54321
-//                        .roles("ADMIN", "USER")
-//                        .build();
-//        return new InMemoryUserDetailsManager(normalUser, adminUser);
-//    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() throws Exception {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(userDetailsService);
-        provider.setPasswordEncoder(passwordEncoder());
-        return provider;
-    }
-
     @Bean
     public UserDetailsService userDetailsService() {
-        return userDetailsService;
+        UserDetails normalUser = User.builder()
+                        .username("NormalUser")
+                        .password("$2a$12$92oMmW2pZUD9WGSk9EpBp.Z.9cD5dXK98j0UMJIM4XgcgQvsiWfSy") //12345
+                        .roles("USER")
+                        .build();
+
+        UserDetails adminUser = User.builder()
+                        .username("AdminUser")
+                        .password("$2a$12$b7zE4gzv0ugqjvMCquxuQeVGwJeZAGAWqvi8/Gpm.QWOlaD9wcsRO") // 54321
+                        .roles("ADMIN", "USER")
+                        .build();
+        return new InMemoryUserDetailsManager(normalUser, adminUser);
     }
+
+
+
 
 
 }
